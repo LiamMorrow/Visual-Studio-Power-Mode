e5e5f8adb981a1872616764580c0b791d4c81b61
Liam Morrow <ldmorrow@outlook.com>
2017-05-19 15:05:33 +1000

    Fixed options not loading on startup

--- PowerModeV2/PowerModePackage.cs
+++ PowerModeV2/PowerModePackage.cs
@@ -69,6 +69,7 @@ namespace PowerMode
         protected override void Initialize()
         {
             base.Initialize();
+            General.LoadSettingsFromStorage();
         }
 
         #endregion
